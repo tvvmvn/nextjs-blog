a01f79a885d705fd83a47e2195961f2871fec068
tvvmvn <tvvmvn@gmail.com>
2025-05-28 11:58:51 +0900

    first commit

---
+++ pages/api/hello.js
@@ -0,0 +1,11 @@
+// req = HTTP incoming message, res = HTTP server response
+export default function handler(req, res) {
+  res.status(200).json({ text: 'Hello' });
+}
+
+// A good use case for API Routes is handling form input. 
+// For example, you can create a form on your page and 
+// have it send a POST request to your API Route. 
+// You can then write code to directly save it to your database. 
+// The API Route code will not be part of your client bundle, 
+// so you can safely write server-side code.
